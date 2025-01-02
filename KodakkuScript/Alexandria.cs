dd59a0484e8c8cef928312d91fda6508e134a91f
Yiwei <kanyeishere@gmail.com>
2025-01-02 23:30:13 +1100

    init commit

---
+++ KodakkuScript/Alexandria.cs
@@ -0,0 +1,275 @@
+﻿using KodakkuAssist.Module.GameEvent;
+using KodakkuAssist.Script;
+using KodakkuAssist.Module.Draw;
+using System.Numerics;
+using Newtonsoft.Json;
+
+namespace KodakkuScript;
+
+[ScriptType(name: "Alexandria", territorys: [1199], guid: "", version: "0.0.0.1",
+    author: "Wotou")]
+public class Omega1
+{
+    [UserSetting("文字横幅提示开关")]
+    public bool isText { get; set; } = true;
+
+    [UserSetting("TTS开关")]
+    public bool isTTS { get; set; } = true;
+
+    [UserSetting("DR TTS开关")]
+    public bool isDRTTS { get; set; } = false;
+    
+    
+    [ScriptMethod(name: "boss1正面扇形", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36379"])]
+    public void boss1正面扇形(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "正面扇形";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(100);
+        dp.Radian = float.Pi / 180 * 120;
+        dp.DestoryAt = 5700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Fan, dp);
+    }
+    
+    [ScriptMethod(name: "boss1正面扇形安全", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36381"])]
+    public void boss1正面扇形安全(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "正面扇形安全";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(100);
+        dp.Radian = float.Pi / 180 * 240;
+        dp.Rotation = float.Pi;
+        dp.DestoryAt = 5700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Fan, dp);
+    }
+    
+    [ScriptMethod(name: "boss2半场刀", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:39136"])]
+    public void boss2半场刀(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "半场刀";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(56.568543f, 28.284272f);
+        dp.DestoryAt = 9800;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp);
+    }
+    
+    [ScriptMethod(name: "boss2辣尾", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36327"])]
+    public void boss2辣尾(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss2辣尾";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(16f, 40f);
+        //dp.Offset = new Vector3(0, 0, 20);
+        dp.DestoryAt = 4700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+    }
+    
+    [ScriptMethod(name: "boss2辣翅", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36330"])]
+    public void boss2辣翅(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss2辣翅1";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(16f, 40f);
+        dp.Offset = new Vector3(12, 0, 0);
+        dp.DestoryAt = 4700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+        var dp1 = accessory.Data.GetDefaultDrawProperties();
+
+        dp1.Name = "boss2辣翅2";
+        dp1.Color = accessory.Data.DefaultDangerColor;
+        dp1.Owner = @event.SourceId();
+        dp1.Scale = new Vector2(16f, 40f);
+        dp1.Offset = new Vector3(-12, 0, 0);
+        dp1.DestoryAt = 4700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp1);
+    }
+    
+    [ScriptMethod(name: "boss2场外直线", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:39138"])]
+    public void boss2场外直线(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss2辣尾";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(10f, 50f);
+        //dp.Offset = new Vector3(0, 0, 20);
+        dp.DestoryAt = 7500;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp);
+    }
+    
+    [ScriptMethod(name: "boss3右刀1", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:39599"])]
+    public void boss3右刀1(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3右刀1";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(20f, 40f);
+        dp.Offset = new Vector3(10, 0, 0);
+        dp.DestoryAt = 4700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+    }
+    
+    [ScriptMethod(name: "boss3右刀2", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36768"])]
+    public void boss3右刀2(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3右刀2";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(20f, 40f);
+        dp.Offset = new Vector3(10, 0, 0);
+        dp.DestoryAt = 6700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+    }
+    
+    [ScriptMethod(name: "boss3右刀3", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:39248"])]
+    public void boss3右刀3(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3右刀3";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(20f, 40f);
+        dp.Offset = new Vector3(10, 0, 0);
+        dp.DestoryAt = 6700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+    }
+    
+    [ScriptMethod(name: "boss3左刀1", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:39247"])]
+    public void boss3左刀1(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3左刀1";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(20f, 40f);
+        dp.Offset = new Vector3(-10, 0, 0);
+        dp.DestoryAt = 6700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+    }
+    
+    [ScriptMethod(name: "boss3左刀2", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:39600"])]
+    public void boss3左刀2(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3左刀2";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(20f, 40f);
+        dp.Offset = new Vector3(-10, 0, 0);
+        dp.DestoryAt = 4700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Straight, dp);
+    }
+    
+    [ScriptMethod(name: "boss3场外直线", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36773"])]
+    public void boss3场外直线(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3场外直线";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new Vector2(10f, 50f);
+
+        dp.DestoryAt = 4700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Rect, dp);
+    }
+    
+    [ScriptMethod(name: "boss3月环", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:36776"])]
+    public void boss3月环(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+        dp.Name = "boss3场外直线";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.Scale = new(40);
+        dp.InnerScale = new(6);
+        dp.DestoryAt = 6700;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Donut, dp);
+    }
+}
+
+public static class EventExtensions
+{
+    private static bool ParseHexId(string? idStr, out uint id)
+    {
+        id = 0;
+        if (string.IsNullOrEmpty(idStr)) return false;
+        try
+        {
+            var idStr2 = idStr.Replace("0x", "");
+            id = uint.Parse(idStr2, System.Globalization.NumberStyles.HexNumber);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public static uint ActionId(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<uint>(@event["ActionId"]);
+    }
+
+    public static uint SourceId(this Event @event)
+    {
+        return ParseHexId(@event["SourceId"], out var id) ? id : 0;
+    }
+
+
+    public static string DurationMilliseconds(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<string>(@event["DurationMilliseconds"]) ?? string.Empty;
+    }
+
+    public static uint SourceRotation(this Event @event)
+    {
+        return ParseHexId(@event["SourceRotation"], out var sourceRotation) ? sourceRotation : 0;
+    }
+
+    public static byte Index(this Event @event)
+    {
+        return (byte)(ParseHexId(@event["Index"], out var index) ? index : 0);
+    }
+
+    public static uint State(this Event @event)
+    {
+        return ParseHexId(@event["State"], out var state) ? state : 0;
+    }
+
+    public static string SourceName(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<string>(@event["SourceName"]) ?? string.Empty;
+    }
+
+    public static uint TargetId(this Event @event)
+    {
+        return ParseHexId(@event["TargetId"], out var id) ? id : 0;
+    }
+
+    public static Vector3 SourcePosition(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<Vector3>(@event["SourcePosition"]);
+    }
+
+    public static Vector3 TargetPosition(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<Vector3>(@event["TargetPosition"]);
+    }
+
+    public static Vector3 EffectPosition(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<Vector3>(@event["EffectPosition"]);
+    }
+}
