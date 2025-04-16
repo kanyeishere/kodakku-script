9d53e5e621ed88b269d663d91a53d78cb63e1d43
Yiwei <kanyeishere@gmail.com>
2025-04-16 23:57:47 +1000

    fix

--- KodakkuScript/Karlin的绝伊甸脚本 (灵视改装版) wotou.cs
+++ KodakkuScript/Karlin的绝伊甸脚本 (灵视改装版) wotou.cs
@@ -28,7 +28,7 @@ namespace CicerosKodakkuAssist.FuturesRewrittenUltimate
     [ScriptType(name: "Karlin的绝伊甸脚本 (灵视改装版) wotou 修改自用",
         territorys: [1238],
         guid: "148718fd-575d-493a-8ac7-1cc7092aff81",
-        version: "0.0.1.24",
+        version: "0.0.1.25",
         note: notesOfTheScript,
         author: "Karlin")]
 
